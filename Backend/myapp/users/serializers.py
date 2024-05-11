99002f6780b32004132bd8777f53d6d5a8cf394e
Habin Kim <habin81111@naver.com>
2024-05-11 21:32:07 +0900

    login api edit

--- Backend/myapp/users/serializers.py
+++ Backend/myapp/users/serializers.py
@@ -9,6 +9,10 @@ class LoginSerializer(serializers.ModelSerializer):
     username = serializers.CharField(required=True)
     password = serializers.CharField(required=True, write_only=True)
 
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
     def validate(self, data):
         user = authenticate(**data)
         if user:
--- Backend/myapp/users/views.py
+++ Backend/myapp/users/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import generics, status
 from rest_framework.response import Response
+import json
 
 from .serializers import RegisterSerializer, LoginSerializer
 
